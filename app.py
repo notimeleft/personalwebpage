539f247b0d428dcccdcb4e7900b042eb3d508164
jerry wang <jerry.wang.ct@gmail.com>
2017-05-12 05:44:33 -0400

    test cv2imwrite

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def hello():
 
 	face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 	eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-	img = cv2.imread('gladys.jpg')
+	img = cv2.imread('barack.jpg')
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
